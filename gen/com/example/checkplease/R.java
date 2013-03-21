fba4e99c6fb6a716711d8727c1f8d243901f2242
unknown <cesar.amaro@20d.mx>
2013-03-21 17:32:01 -0600

    avance calculadora

--- gen/com/example/checkplease/R.java
+++ gen/com/example/checkplease/R.java
@@ -21,13 +21,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int bg=0x7f020001;
+        public static final int green_button=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int red_button=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int editText1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int calculadora=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
