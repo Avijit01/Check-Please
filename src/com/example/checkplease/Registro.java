078dc8a39d074e6734f7e63ce1d4b88c1185a8b0
Jose Raul Gomez Montemayor <A00805403>
2013-04-01 12:56:23 -0600

    cambio de pantallas en vistas

---
+++ src/com/example/checkplease/Registro.java
@@ -0,0 +1,15 @@
+package com.example.checkplease;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Registro extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.forma_de_pago);
+	}
+
+}
