8203f9795aa412103783a0be5262ad29668ce66f
Mario Trujillo <mtru92@gmail.com>
2013-04-04 20:19:00 -0600

    Added Fb button and feedback

--- src/com/example/checkplease/Lista.java
+++ src/com/example/checkplease/Lista.java
@@ -4,19 +4,32 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Lista  extends Activity{
+public class Lista extends Activity implements OnClickListener {
 	
-	EditText etTip;
+	// Definicion de los botones presentes en la vista
 	Button regresa;
+	Button invitar;
+	ImageButton agregar;
+	ImageButton facebook;
+	ImageButton eliminar;
+	
+	// Variables que maneja la vista para calculos
+	EditText etTip;
 	float gTotal;
 	float falta;
 	
@@ -26,7 +39,30 @@ public class Lista  extends Activity{
 		setContentView(R.layout.lista_usuarios);
 		TextView titulo = (TextView)findViewById(R.id.titulo);
 		titulo.setText("Pago Individual");
+		
 		regresa = (Button)findViewById(R.id.regresabtn);
+		invitar = (Button)findViewById(R.id.bInvitar);
+		agregar = (ImageButton)findViewById(R.id.bAgregar);
+		facebook = (ImageButton)findViewById(R.id.bFacebook);
+		eliminar = (ImageButton)findViewById(R.id.bEliminar);
+		
+		// Agregar click listener
+		agregar.setOnClickListener(this);
+		invitar.setOnClickListener(this);
+		eliminar.setOnClickListener(this);
+		facebook.setOnClickListener(this);
+		facebook.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View arg0, MotionEvent me) {
+				if (me.getAction() == MotionEvent.ACTION_DOWN) {
+					facebook.setColorFilter(Color.argb(100, 0, 0, 0));
+					dispatchTouchEvent(me);
+				} else if (me.getAction() == MotionEvent.ACTION_UP) {
+					facebook.setColorFilter(null); // or null
+				}
+				return false;
+			}
+		 });
         
 		ArrayList<Person> usuarios = new ArrayList<Person>();
 		usuarios.add(new Person("Derp", 120.0f, false));
@@ -46,7 +82,7 @@ public class Lista  extends Activity{
 		}
 		tvgTotal.setText(String.valueOf(gTotal));
 		tvFalta.setText(String.valueOf(falta));
-		regresa.setOnClickListener(new  View.OnClickListener(){
+		regresa.setOnClickListener(new View.OnClickListener(){
         	public void onClick(View view){
         		Lista.this.finish();
         	}
@@ -66,12 +102,30 @@ public class Lista  extends Activity{
     public boolean onOptionsItemSelected(MenuItem item) {
         //respond to menu item selection
     	switch (item.getItemId()) {
-        case R.id.acerca://se cierra el menu
-        	startActivity(new Intent(this, Acerca.class));
-        return true;
-        
-        default:
-        return super.onOptionsItemSelected(item);
+    	case R.id.acerca://se cierra el menu
+    		startActivity(new Intent(this, Acerca.class));
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
-    }
+	@Override
+	public void onClick(View v) {
+		Toast toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+		switch(v.getId()) {
+		case R.id.bAgregar:
+			toast = Toast.makeText(this, "Agregar una persona", Toast.LENGTH_SHORT);
+			break;
+		case R.id.bInvitar:
+			toast = Toast.makeText(this, "Invitar a un amigo", Toast.LENGTH_SHORT);
+			break;
+		case R.id.bFacebook:
+			toast = Toast.makeText(this, "Compartir en Facebook", Toast.LENGTH_SHORT);
+			break;
+		case R.id.bEliminar:
+			toast = Toast.makeText(this, "Eliminar persona(s)", Toast.LENGTH_SHORT);
+			break;
+		}
+		toast.show();
+	}
 }
